035eb066c4ba0d5c970571f12594d887ae9c795a
Ian Chen <ianchen06@gmail.com>
2023-11-01 04:12:29 +0000

    add logo and how to play

--- cli.py
+++ cli.py
@@ -1,5 +1,24 @@
 from logic import get_new_board, check_winner
 
+logo = """
+___________.__         ___________               ___________            
+\__    ___/|__| ____   \__    ___/____    ____   \__    ___/___   ____  
+  |    |   |  |/ ___\    |    |  \__  \ _/ ___\    |    | /  _ \_/ __ \ 
+  |    |   |  \  \___    |    |   / __ \\\  \___    |    |(  <_> )  ___/ 
+  |____|   |__|\___  >   |____|  (____  /\___  >   |____| \____/ \___  >
+                   \/                 \/     \/                      \/ 
+                   
+How to play:
+    player O > row_num, col_num
+    player O > 0,0
+
+    ['O', ' ', ' ']
+    [' ', ' ', ' ']
+    [' ', ' ', ' ']
+
+=========================================================================
+"""
+
 
 def print_board(board):
     for row in board:
@@ -11,7 +30,7 @@ if __name__ == "__main__":
     board = get_new_board()
     current_player = 'O'
     winner = None
-    print("we have a new board")
+    print(logo)
     while winner is None:
         print_board(board)
         player_input = input(f"player {current_player} > ")
